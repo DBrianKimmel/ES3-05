84007f7fa53e5863f67773e1d08ea03796abac86
DBrianKimmel <D.BrianKimmel@gmail.com>
2026-06-13 12:54:28 -0400

    Add test cases

---
+++ test/test_wifi/test_wifi_sta.cpp
@@ -0,0 +1,21 @@
+//
+
+void setup () {
+    delay(2000); // Wait for 2 seconds before starting the tests
+}
+
+void loop () {
+    // Run the unit tests
+    UNITY_BEGIN();
+    // Add test cases here, e.g.:
+    // RUN_TEST(test_wifi_connection_timeout);
+    // RUN_TEST(test_get_ip_address_disconnected);
+    UNITY_END();
+
+    // Stop the loop after running tests
+    while (true) {
+        delay(1000);
+    }
+}
+
+// ### END DBK
